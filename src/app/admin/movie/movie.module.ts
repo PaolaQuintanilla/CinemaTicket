269031e98a6e69f5953658a2db98b9d36a73d0e7
Paola.Quintanilla <74014.linares@gmail.com>
2021-05-20 09:23:06 -0300

    adding movie module

---
+++ src/app/admin/movie/movie.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MovieRoutingModule } from './movie-routing.module';
+import { CreateMovieComponent } from './create-movie/create-movie.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    CreateMovieComponent
+  ],
+  imports: [
+    CommonModule,
+    MovieRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class MovieModule { }
